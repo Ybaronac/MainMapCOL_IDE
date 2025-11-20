d4116604f316f7c1a0d335b49fead6010e00ed94
Ybaronac <yemibace@gmail.com>
2025-11-20 23:10:55 +0100

    se realiza la actualizacion de la pagina de recursos y se agregan iconos al menu de navegacion

--- my-react-app/src/components/Resources.jsx
+++ my-react-app/src/components/Resources.jsx
@@ -7,32 +7,28 @@ const Resources = () => (
   <div className="min-h-screen bg-gray-50 py-8">
     <TextSection
       content={[
-        { type: 'title', text: WebpageContent.description_title1 },
-        { type: 'paragraph', text: WebpageContent.description_paragraph1 }
+        { type: 'title', text: WebpageContent.resources_title1 },
+        { type: 'paragraph', text: WebpageContent.resources_paragraph1 }
       ]}
     />
 
     <TextSection
       content={[
-        { type: 'title', text: WebpageContent.description_title2 },
-        { type: 'paragraph', text: WebpageContent.description_paragraph2 },
-        { type: 'subtitle', text: WebpageContent.description_title3 },
-        { type: 'paragraph', text: WebpageContent.description_paragraph3 },
-        { type: 'subtitle', text: WebpageContent.description_title4 },
-        { type: 'paragraph', text: WebpageContent.description_paragraph4 },
-        { type: 'subtitle', text: WebpageContent.description_title5 },
-        { type: 'paragraph', text: WebpageContent.description_paragraph5 },
-        { type: 'subtitle', text: WebpageContent.description_title6 },
-        { type: 'paragraph', text: WebpageContent.description_paragraph6 }
+        { type: 'title', text: WebpageContent.resources_title2 },
+        { type: 'paragraph', text: WebpageContent.resources_paragraph2 },
+        { type: 'subtitle', text: WebpageContent.resources_title3 },
+        { type: 'paragraph', text: WebpageContent.resources_paragraph3 },
+        { type: 'subtitle', text: WebpageContent.resources_title4 },
+        { type: 'paragraph', text: WebpageContent.resources_paragraph4 },
+        { type: 'subtitle', text: WebpageContent.resources_title5 },
+        { type: 'paragraph', text: WebpageContent.resources_paragraph5 },
+        { type: 'subtitle', text: WebpageContent.resources_title6 },
+        { type: 'paragraph', text: WebpageContent.resources_paragraph6 },
+        { type: 'subtitle', text: WebpageContent.resources_title7 },
+        { type: 'paragraph', text: WebpageContent.resources_paragraph7 }
       ]}
     />
-
-    <TextSection
-      content={[
-        { type: 'title', text: WebpageContent.description_title7 },
-        { type: 'paragraph', text: WebpageContent.description_paragraph7 }
-      ]}
-    />
+    
 
     <div className="max-w-4xl w-full mx-auto p-4">
       <Link to="/" className="inline-block mt-4 text-blue-600 hover:underline">
