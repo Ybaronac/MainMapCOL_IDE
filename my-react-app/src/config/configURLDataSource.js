422b4f67dfeba514fe76c8da0de06a35df996c54
Ybaronac <yemibace@gmail.com>
2025-11-28 01:37:46 +0100

    se actualizan todos los tados a sus valores reales para su revision

--- my-react-app/src/config/configURLDataSource.js
+++ my-react-app/src/config/configURLDataSource.js
@@ -4,16 +4,24 @@ export const COLOMBIA_DEPARTMENTS_MAP_JSON_DATA = 'https://gist.githubuserconten
 export const ETC_MAP_2025_JSON_DATA = 'https://gist.githubusercontent.com/Ybaronac/87f4b0f70c37b1285ebe1890a30efefa/raw/ce0b008193a0a9d6d3451ef8e9b6ae2a6e552fbc/MapETCv3.json';
 
 //Configuracion de URL - IDE Data Map
-export const IDE_COLOMBIA_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/df3edd8c29daa4855a6617b81f52a1cf/raw/94185353fb8a4900c11eb86f61695e4877bcda7c/IDE_ColGeneralTest.json';
-export const IDE_DEPARTMENTS_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/ea35627ef169446ee9624421eef8e4fd/raw/544b2eb70a1904f9952f8e3ed651a0e3272f0e82/IDE_ColTest.json';
+//Test
+//export const IDE_COLOMBIA_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/df3edd8c29daa4855a6617b81f52a1cf/raw/94185353fb8a4900c11eb86f61695e4877bcda7c/IDE_ColGeneralTest.json';
+//export const IDE_DEPARTMENTS_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/ea35627ef169446ee9624421eef8e4fd/raw/544b2eb70a1904f9952f8e3ed651a0e3272f0e82/IDE_ColTest.json';
 //export const IDE_ETC_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/762c1259237d823d5983a67879850ff1/raw/52d2aa2d3b3322de17fa225c336b4ba33058f566/IDE_ETC_CHOROPLETH.json';
-export const IDE_ETC_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/a9692b34f65aff18440f1d7701a22e92/raw/4160e1b5753063b4bf8235fc3fabf0f4f0b5e85e/IDE_choropleth_realData.json';
+//Real
+export const IDE_COLOMBIA_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/df3edd8c29daa4855a6617b81f52a1cf/raw/94185353fb8a4900c11eb86f61695e4877bcda7c/IDE_ColGeneralTest.json';
+//export const IDE_DEPARTMENTS_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/ea35627ef169446ee9624421eef8e4fd/raw/544b2eb70a1904f9952f8e3ed651a0e3272f0e82/IDE_ColTest.json';
+export const IDE_ETC_CHOROPLETH = 'https://gist.githubusercontent.com/Ybaronac/a9692b34f65aff18440f1d7701a22e92/raw/3dc9e8dd7a8f230ef5a8aec4f0cdae6dd5094ba9/IDE_choropleth_realData.json';
 
 
 //Configuracion de URL - Sub indices Data
+//Test
 //export const ITEMS_IDE = 'https://gist.githubusercontent.com/Ybaronac/13ade4e7376c56e3b5b9ae439cfa9439/raw/b775c91fbd47acc2a9c031b550cb05be2d643e8f/ItemsIDE.json';
-export const ITEMS_IDE = 'https://gist.githubusercontent.com/Ybaronac/26c41cb8e51e0f4c7c7abddfb8905ca4/raw/3430bd7cbbaba5eb4fb96d4cbe1cb09addba423c/ItemsIDEV2.json';
-
 //export const DEPARTMENTS_ITEMS = 'https://gist.githubusercontent.com/Ybaronac/26018c1622e630cd970043a5f664ff46/raw/5df603fb5239b91d8316b215c47e32ffd10d91cb/DepartmentsItemsIDEperYearTest.json';
-export const DEPARTMENTS_ITEMS = 'https://gist.githubusercontent.com/Ybaronac/10783204ae6c4313b858588e5bdd9fbc/raw/e5547853e79daaba0777bb6326b28304aa27db04/DepartmentsItemsIDEperYearTestV2.json';
-export const ETC_ITEMS = 'https://gist.githubusercontent.com/Ybaronac/217f5e45cd2dc24a88d9a06f8cce1d31/raw/8c13ced029020c80fc9a201a257e91dcdcd139b0/ETC_ITEMS.json';
+
+
+
+//Real
+//export const DEPARTMENTS_ITEMS = 'https://gist.githubusercontent.com/Ybaronac/10783204ae6c4313b858588e5bdd9fbc/raw/e5547853e79daaba0777bb6326b28304aa27db04/DepartmentsItemsIDEperYearTestV2.json';
+export const ITEMS_IDE = 'https://gist.githubusercontent.com/Ybaronac/b572dca32170a829907d9579f4229b01/raw/9fa80aeb98380d48711588752488c321a135d9da/ItemsETCMapReal.json';
+export const ETC_ITEMS = 'https://gist.githubusercontent.com/Ybaronac/a0437833d5e02c13174639ef752be0d5/raw/fe414ca5e1d6324d0d6f268ad8cd21c431763007/ETC_Items_Real.json';
